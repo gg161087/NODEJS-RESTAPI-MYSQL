ec9e4995d54e08b80f2b5cf19522b13bdd8b065e
Gonzalo <gonzalo.gonzalez@sg.gba.gob.ar>
2022-12-01 15:03:30 -0300

    consts

--- src/conection_db.js
+++ src/conection_db.js
@@ -1,9 +1,10 @@
 import {createPool} from 'mysql2/promise'
+const GLOBAL_CONSTS = require('../src/const/global_consts');
 
 export const conn = createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
+    host: GLOBAL_CONSTS.PORT,
+    user: GLOBAL_CONSTS.DB_USERNAME,
+    password: GLOBAL_CONSTS.DB_PASSWORD,
     port: 3306,
-    database: 'db_hospital'
+    database: GLOBAL_CONSTS.DB_NAME
 });
---
+++ src/const/global_consts.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+
+module.exports = {
+    PORT: process.env.PORT || 5050,
+    DB_NAME: process.env.DB_NAME,
+    DB_USERNAME: process.env.DB_USERNAME,
+    DB_PASSWORD: process.env.DB_PASSWORD
+}
